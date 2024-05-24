d84ccaad241853c9b96a05bc27f97493d470d1b4
Kia Neitzell <kia.neitzell@gmail.com>
2024-05-24 11:12:56 -0700

    update

--- server/models.py
+++ server/models.py
@@ -1,8 +1,8 @@
+from config import bcrypt, db
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-from config import db, bcrypt
 
 
 # Models go here!
