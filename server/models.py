b90212ca5d8a9795d98ee45cbde65e25dfc1b6ec
Kia Neitzell <kia.neitzell@gmail.com>
2024-05-23 20:44:25 -0700

    updated tables

--- server/models.py
+++ server/models.py
@@ -1,18 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
-
-from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 # Models go here!
-class User(db.Model):
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = "customer"
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
     _password_hash = db.Column(db.String(128), nullable=False)
+
+    # Relationships
+    bookings = db.relationship("Booking", back_populates="customer")
+    packages = db.relationship("Package", back_populates="customer")
+
 
     @hybrid_property
     def password_hash(self):
@@ -25,25 +28,36 @@ class User(db.Model):
 
     def authenticate(self, password_to_check):
         return check_password_hash(self._password_hash, password_to_check)
-class Animal(db.Model):
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = "animals"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     species = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    
+    # Relationships
+    packages = db.relationship("Package", back_populates="animal")   
 
-    owner = db.relationship('Customer', back_populates='animals')
-
-class Booking(db.Model):
+class Booking(db.Model,  SerializerMixin):
+    __tablename__ = "booking"
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     check_in_date = db.Column(db.Date, nullable=False)
     check_out_date = db.Column(db.Date, nullable=False)
-    animals = db.relationship('Animal', secondary='animal_booking', back_populates='bookings')
+    
+    # Relationships
+    customer = db.relationship("Customer", back_populates="bookings")
+    packages = db.relationship("Package", back_populates="booking")
 
-    customer = db.relationship('Customer', back_populates='bookings')
-class AnimalBooking(db.Model):
+class Package(db.Model,  SerializerMixin):
+    __tablename__ = "package"
     id = db.Column(db.Integer, primary_key=True)
     booking_id = db.Column(db.Integer, db.ForeignKey('booking.id'), nullable=False)
     animal_id = db.Column(db.Integer, db.ForeignKey('animal.id'), nullable=False)
-    notes = db.Column(db.Text)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'),nullable=False)
+
+    # Relationships
+    customer = db.relationship("Customer", back_populates="packages")
+    animal = db.relationship("Animal", back_populates="packages")
+    booking = db.relationship("Booking", back_populates="packages")
