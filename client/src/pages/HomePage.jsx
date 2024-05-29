6045f657be3792f570ac4ff70b7bcbbeb9749447
Kia Neitzell <kia.neitzell@gmail.com>
2024-05-28 23:01:54 -0700

    added logo.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import AddAnimal from '../components/Animals/AnimalForm';
+import foxLogo from '/Users/kianeitzell/development/code/phase-4/p4-group-project/client/src/components/fox.png'; // Import the image
 
 function HomePage({ user }) {
+
+
     return (
         <>
             <header>
-                <h1>Hello{user ? `, ${user.userName}` : null}. Welcome to PetPal Hotel</h1>
+                <h1>Hello{user ? `, ${user.userName}` : null}. Welcome to PetPal Hotel!</h1>
+                <img src={foxLogo} alt="Fox Logo" style={{ width: '100px', height: 'auto', position: 'absolute', top: '0', right:  '30px' }} />
             </header>
 
             <nav>
