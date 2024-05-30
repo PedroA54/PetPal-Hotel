d547672dab0c660a1edcbdce55151eb70d1f9ba3
Kia Neitzell <kia.neitzell@gmail.com>
2024-05-30 16:49:45 -0700

    styling changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,8 +12,9 @@ function HomePage({ user }) {
     return (
         <div className="home-page-container">
             <header>
-                <h1>Hello{user ? `, ${user.userName}` : null}. Welcome to PetPal Hotel!</h1>
+            <h1>Hello{user ? `, ${user.userName}` : null}. Welcome to PetPal Hotel!</h1>
                 <img src={logo} alt="Logo" className="home-logo" />
+                <p className="welcome-paragraph">At PetPal Hotel, we are dedicated to providing the best care for your beloved pets. Whether it's a cozy stay, a playful day, or a grooming session, we ensure a tail-wagging experience for every furry friend. Explore our services and make your pet's stay memorable!</p>
             </header>
             <div className="animal-row">
                 <section>
