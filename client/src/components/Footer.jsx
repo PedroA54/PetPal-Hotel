61700d7355132bd1125c1f7a79dad034ba359103
Pedro Alarcon <pedroa54@icloud.com>
2024-06-03 12:37:38 +0200

    final touches on footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
 	return (
 		<footer>
 			<p>PetPal Hotel was built by
-            <a href='https://github.com/PedroA54' rel='noreferrer noopener' target='_blank'> Pedro Alarcon</a>,&nbsp;
-				, and
+            <a href='https://github.com/PedroA54' rel='noreferrer noopener' target='_blank'> Pedro Alarcon</a>&nbsp;
+            and 
 				<a href='https://github.com/kia659' rel='noreferrer noopener' target='_blank'> Kia 
 				</a>&nbsp; for educational purposes as part of
 				<a href='https://flatironschool.com/' rel='noreferrer noopener' target='_blank'> Flatiron School</a>&apos;s Software Engineering bootcamp.
