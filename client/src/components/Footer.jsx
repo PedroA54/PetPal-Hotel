d89c5ab61c574b9da984a763db447456d5f3575e
Pedro Alarcon <pedroa54@icloud.com>
2024-07-30 12:00:03 +0200

    adds footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
 	return (
-		<footer>
+		<footer className="footer">
 			<p>PetPal Hotel was built by
             <a href='https://github.com/PedroA54' rel='noreferrer noopener' target='_blank'> Pedro Alarcon</a>&nbsp;
             and 
 				<a href='https://github.com/kia659' rel='noreferrer noopener' target='_blank'> Kia 
-				</a>&nbsp; for educational purposes as part of
-				<a href='https://flatironschool.com/' rel='noreferrer noopener' target='_blank'> Flatiron School</a>&apos;s Software Engineering bootcamp.
+				</a>&nbsp; for educational purposes.
+				
 			</p>
 		</footer>
 )}
